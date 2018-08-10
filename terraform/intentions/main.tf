6b9ed2bfb365e8a5b6f62fbdbae714f67618e71c
Dave Arnold <darnold@hashicorp.com>
2018-08-10 15:06:08 -0700

    fixing resource naming

--- terraform/intentions/main.tf
+++ terraform/intentions/main.tf
@@ -29,21 +29,21 @@ resource "consul_intention" "deny_product" {
     action           = "deny"
 }
 
-resource "consul_intention" "deny_product" {
+resource "consul_intention" "allow_product" {
     count = "${var.deny_product == true ? 0 : 1}"
     source_name      = "web_client"
     destination_name = "product"
     action           = "allow"
 }
 
-resource "consul_intention" "database" {
+resource "consul_intention" "deny_listing" {
     count = "${var.deny_listing == true ? 1 : 0}"
     source_name      = "web_client"
     destination_name = "listing"
     action           = "deny"
 }
 
-resource "consul_intention" "database" {
+resource "consul_intention" "allow_listing" {
     count = "${var.deny_listing == true ? 0 : 1}"
     source_name      = "web_client"
     destination_name = "listing"
